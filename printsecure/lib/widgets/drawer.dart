5849be7b2560d132a36edb5e478a22e4c1c227a5
Kushagra Goel <kushagra.goel@gmail.com>
2022-09-21 02:19:32 +0530

    userProfile drawer complete UI

--- printsecure/lib/widgets/drawer.dart
+++ printsecure/lib/widgets/drawer.dart
@@ -31,15 +31,93 @@ class MyDrawer extends StatelessWidget {
           Positioned(
             height: 100,
             child: CircleAvatar(
-              backgroundColor: Color.fromARGB(255, 247, 222, 1),
+              backgroundColor: Color.fromARGB(255, 217, 217, 215),
               maxRadius: 80,
             ),
           ),
+          SizedBox(
+            height: 40,
+          ),
 
           Container(
+            child: Column(
+              children: [
+                Text(
+                  "Username",
+                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Useremailid@gmail.com",
+                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Center(
+                  child: ElevatedButton.icon(
+                    // child: Text(
+                    //   'Change Password',
+                    //   style: TextStyle(color: Colors.black),
+                    // ),
+                    style: ElevatedButton.styleFrom(
+                      side: BorderSide(color: Colors.lightBlue),
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15))),
+                    ),
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.key,
+                      color: Colors.black,
+                      size: 24.0,
+                    ),
+                    label: Text(
+                      'Change Password',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: ElevatedButton.icon(
+                    // child: Text(
+                    //   'Change Password',
+                    //   style: TextStyle(color: Colors.black),
+                    // ),
+                    style: ElevatedButton.styleFrom(
+                      side: BorderSide(color: Colors.lightBlue),
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15))),
+                    ),
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.print,
+                      color: Colors.black,
+                      size: 24.0,
+                    ),
+                    label: Text(
+                      'About PrintSecure',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Want to create QR code, create account as vendor",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.grey),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Image.asset("assets/login/Rectangle 83.png"),
+                Image.asset("assets/login/Rectangle 82.png"),
+              ],
+            ),
             margin: EdgeInsets.fromLTRB(30, 0, 30, 100),
-            height: 350,
-            
+            height: 270,
             decoration: BoxDecoration(
                 color: Colors.white,
                 border: Border.all(color: Colors.black),
@@ -56,21 +134,47 @@ class MyDrawer extends StatelessWidget {
           Row(
             children: [
               Padding(padding: EdgeInsets.fromLTRB(65, 25, 50, 60)),
-              ElevatedButton(
-                child: Text(
-                  'Logout',
-                  style: TextStyle(color: Colors.black),
+              Center(
+                child: ElevatedButton.icon(
+                  // child: Text(
+                  //   'Change Password',
+                  //   style: TextStyle(color: Colors.black),
+                  // ),
+                  style: ElevatedButton.styleFrom(
+                    side: BorderSide(color: Colors.lightBlue),
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                  ),
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.logout,
+                    color: Colors.black,
+                    size: 24.0,
+                  ),
+                  label: Text(
+                    'Logout',
+                    style: TextStyle(color: Colors.black),
+                  ),
                 ),
-                style: ElevatedButton.styleFrom(
-                  side: BorderSide(width: 2, color: Colors.black),
-                  backgroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15))),
-                ),
-                onPressed: () {},
               ),
             ],
           ),
+
+          Row(
+            children: [
+              Text(
+                "                          secured by ",
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "Print Secure",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Colors.blueGrey),
+              ),
+            ],
+          )
         ],
       ),
     );
